cade69af60c3c62c60ae28a7607d1040dc046761
Mike Horn <gpbmike@gmail.com>
2014-09-11 16:26:03 -0700

    reorg

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,31 +15,50 @@ module.exports = function(grunt) {
         livereload: true
       },
       scripts: {
-        files: ['app/**/*.js', 'i18n/**/*.js'],
+        files: ['app/**/*.js', 'lib/**/*.js'],
         tasks: ['jshint']
       },
       html: {
         files: ['app/**/*.html']
       },
       translations: {
-        files: ['i18n/**/*.json'],
-        tasks: ['jsonlint']
+        files: ['translations/**/*.json'],
+        tasks: ['jsonlint', 'concat']
       }
     },
     jshint: {
-      uses_defaults: ['app/**/*.js', 'i18n/**/*.js']
+      uses_defaults: ['app/**/*.js', 'lib/**/*.js']
     },
     jsonlint: {
-      uses_defaults: ['i18n/**/*.json']
+      uses_defaults: ['translations/**/*.json']
+    },
+    concat: {
+      translations: {
+        options: {
+          banner: '/* Generated file, do not edit. */\n' +
+            'Ember.ENV.I18N_SUPPORTED_LANGUAGES = [];\n' +
+            'Ember.ENV.translations={};\n',
+          process: function (src, filepath) {
+            var language = filepath.split('/').pop().split('.')[0];
+            var supported = 'Ember.ENV.I18N_SUPPORTED_LANGUAGES.push(\'' + language + '\')';
+            var key = 'Ember.ENV.translations.' + language;
+            var value = JSON.stringify(JSON.parse(src));
+            return supported + ';\n' + key + '=' + value + ';';
+          }
+        },
+        src: ['translations/**/*.json'],
+        dest: 'lib/translations.js'
+      }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-jsonlint');
 
-  grunt.registerTask('serve', ['jshint', 'jsonlint', 'connect', 'watch']);
+  grunt.registerTask('serve', ['jshint', 'jsonlint', 'concat', 'connect', 'watch']);
 
   grunt.registerTask('default', ['serve']);
 
